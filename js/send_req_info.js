245050b20de06d9287c0433ab84e1cc53430db18
PPLong222 <PPLong222@outlook.com>
2022-11-26 16:41:21 +0800

    fix: fix a error in js file

--- js/send_req_info.js
+++ js/send_req_info.js
@@ -1,7 +1,7 @@
 // 获取IP地址
 $.ajax({
 	type: "GET",	
-	url: "http://43.139.93.91:8080/getIp",
+	url: "http://43.139.93.91:8080/net/getIp",
 	dataType: "json",
 	success: function(data) {
 		//请求成功后回调函数
